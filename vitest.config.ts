ab1510a05f263b73647464070e82b4bc02f76391
Trevor Swanson <t.swanson27@gmail.com>
2026-05-30 20:12:44 -0400

    ci: remove tsconfig path plugin from vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from "vitest/config";
-import tsconfigPaths from "vite-tsconfig-paths";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [tsconfigPaths(), react()],
+  plugins: [react()],
+  resolve: {
+    tsconfigPaths: true
+  },
   test: {
     environment: "node",
     globals: true,
